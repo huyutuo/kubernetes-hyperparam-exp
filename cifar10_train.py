c87ae1ca7ce0f30a92933554cbe7e1c9ccf50338
huyutuo <923366850@qq.com>
2019-01-09 14:40:52 +0800

    updata DATA_DIR

--- cifar10_train.py
+++ cifar10_train.py
@@ -74,8 +74,7 @@ class TSVLogger():
    
 def main():
     job_id = int(os.environ['JOB_ID'])
-    DATA_DIR = '/datasets/cifar10-data'
-
+    DATA_DIR='/path/to/dataset'
     #print('Downloading datasets')
     train_set_raw = torchvision.datasets.CIFAR10(root=DATA_DIR, train=True, download=False)
     test_set_raw = torchvision.datasets.CIFAR10(root=DATA_DIR, train=False, download=False)
